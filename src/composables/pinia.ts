37c5018e6843e7010e5d9690eafaebaea945f48b
TTiip <2711430287@qq.com>
2022-09-29 19:28:52 +0800

    feat: 添加 tagView 组件数据持久化功能

--- src/composables/pinia.ts
+++ src/composables/pinia.ts
@@ -3,7 +3,7 @@
 const getTagsViewInstance = () => {
   const tagsViewStore = useTagsViewStore()
 
-  tagsViewStore.$subscribe((mutation, state) => {
+  tagsViewStore.$subscribe((_, state) => {
     // 每当它发生变化时，将整个状态持久化到本地存储
     localStorage.setItem('visitedViews', JSON.stringify(state.visitedViews))
   })
