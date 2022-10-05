e9e463d2162a697c7bde6c4d3c62b973c59f3ce3
TTiip <2711430287@qq.com>
2022-10-06 01:54:51 +0800

    fix: 修复 tagsView 组件属性不存在问题。

--- src/composables/pinia.ts
+++ src/composables/pinia.ts
@@ -3,9 +3,14 @@
 const getTagsViewInstance = () => {
   const tagsViewStore = useTagsViewStore()
 
+  // 每当它发生变化时，将整个状态持久化到本地存储
   tagsViewStore.$subscribe((_, state) => {
-    // 每当它发生变化时，将整个状态持久化到本地存储
-    localStorage.setItem('visitedViews', JSON.stringify(state.visitedViews))
+    // // 删除matched 防止JSON.stringify 格式化报错
+    const coverVisitedViews = JSON.stringify(state.visitedViews.map((item: any) => ({
+      ...item,
+      matched: []
+    })))
+    localStorage.setItem('visitedViews', coverVisitedViews)
   })
 
   return tagsViewStore
