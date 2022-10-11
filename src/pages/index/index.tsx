ec761712e783961fff7cb2a9c4ba07e1d7a6b8be
TTiip <2711430287@qq.com>
2022-10-11 20:52:10 +0800

    feat: 添加 刷新清除 缓存功能

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -10,14 +10,14 @@
 export default defineComponent({
   name: 'index',
   setup () {
-    const str = ref<string>('')
-    function addStr () {
-      str.value += '-'
+    const num = ref<number>(0)
+    function addNum () {
+      num.value += 1
     }
     return () => (
       <div>
-        <h1 onClick={ addStr }>
-          Index - { str.value }
+        <h1 onClick={ addNum }>
+          Index - { num.value }
         </h1>
       </div>
     )
