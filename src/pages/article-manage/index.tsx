ee02c40cda6bb1dd45a85c9d03d5b417cb7cb8e6
TTiip <2711430287@qq.com>
2022-10-12 00:04:54 +0800

    fix: 修复定位导致的拖拽动画不显示问题。

--- src/pages/article-manage/index.tsx
+++ src/pages/article-manage/index.tsx
@@ -7,84 +7,13 @@
   </route>
 */
 
-import Sortable from 'sortablejs'
-
 export default defineComponent({
   name: 'article-manage',
   setup () {
-    onMounted(() => {
-      /* ===== DRAG and DROP ===== */
-      const dropItems: any = document.getElementById('drop-items')
-
-      // eslint-disable-next-line no-new
-      new Sortable(dropItems, {
-        animation: 350,
-        chosenClass: 'sortable-chosen',
-        dragClass: 'sortable-drag'
-      })
-    })
     return () => (
-      <div class="drop__container flex" id="drop-items">
-        <div class="drop__card">
-          <div class="drop__data">
-            <div>
-              <h1 class="drop__name">Clay</h1>
-              <span class="drop__profession">Web developer</span>
-            </div>
-          </div>
-
-          <div>
-            <a href="#" class="drop__social"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-twitter"></i></a>
-          </div>
-        </div>
-
-        <div class="drop__card">
-          <div class="drop__data">
-            <div>
-              <h1 class="drop__name">Sara Mill</h1>
-              <span class="drop__profession">Graphic design</span>
-            </div>
-          </div>
-
-          <div>
-            <a href="#" class="drop__social"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-twitter"></i></a>
-          </div>
-        </div>
-
-        <div class="drop__card">
-          <div class="drop__data">
-            <div>
-              <h1 class="drop__name">Robbie Ford</h1>
-              <span class="drop__profession">Ui/Ux design</span>
-            </div>
-          </div>
-
-          <div>
-            <a href="#" class="drop__social"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-twitter"></i></a>
-          </div>
-        </div>
-
-        <div class="drop__card">
-          <div class="drop__data">
-            <div>
-              <h1 class="drop__name">Jenny Lit</h1>
-              <span class="drop__profession">Digital marketing</span>
-            </div>
-          </div>
-
-          <div>
-            <a href="#" class="drop__social"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="drop__social"><i class="bx bxl-twitter"></i></a>
-          </div>
-        </div>
-      </div>
+      <h1>
+				Article
+      </h1>
     )
   }
 })
